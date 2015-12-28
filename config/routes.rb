ad4cf9ff725e7f23ae7750de59dab559f977c00b
codingInSpace <jonathan.grangien@gmail.com>
2015-12-28 23:24:50 +0100

    Add login logout links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users,  :controllers => { :registrations => "users/registrations" }
+  resources :users
   resources :articles do
     resources :comments
   end
