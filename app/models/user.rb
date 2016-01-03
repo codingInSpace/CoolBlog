9d1cce53ce6253e9c4742d1309222cb9fd3099cd
codingInSpace <jonathan.grangien@gmail.com>
2016-01-03 14:23:36 +0100

    Implement articles user association on models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_and_belongs_to_many :roles
+  has_many :articles
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
